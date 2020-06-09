3bc4a991c9125b6e8f79a177466c67a85d71d892
dmitry <dmitry@dtech-ural.ru>
2020-06-09 13:42:50 +0500

    task4 completed

---
+++ Lesson1/task4/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Выполнил Ищенко Дмитрий
+            // 4. Написать программу нахождения корней заданного квадратного уравнения.
+            Console.WriteLine("Задача 4: \n 4. Написать программу нахождения корней заданного квадратного уравнения.");
+            double a, b, c;
+            Console.WriteLine("Квадратное уравнение   —   это уравнение вида: ax 2 + bx + c = 0");
+            do
+            {
+                ConsoleInput("Введите первый или старший коэффициент (a) не равный 0: ", out a);
+                if (a == 0) Console.WriteLine($"(a) = {a}, (a) не должен равнятся 0 ");
+                else break;
+            } while (true);
+            ConsoleInput("Введите второй коэффициент или коэффициент при х (b): ", out b);
+            ConsoleInput("Введите свободный член (c): ", out c);
+            double d;
+            d = (b * b) - (4 * a * c);
+            if (d < 0) Console.WriteLine($"D = {d} < 0, уровнение не имеет корней");
+            else if (d == 0) Console.WriteLine($"D = {d} == 0, квадратное уравнение имеет один корень X = -{b}/2{a}");
+            else Console.WriteLine($"D = {d} > 0, уравнение имеет два корня:\n\tX1 = (-{b} + √{d})/2{a} \t X2 = (-{b} - √{d})/2{a}");
+            Console.ReadKey();
+
+        }
+        static void ConsoleInput(string msg, out double arg)
+        {
+            do
+            {
+                Console.Write(msg);
+                if (Double.TryParse(Console.ReadLine(), out arg)) break;
+                else Console.WriteLine("Введенное значение, не удалось пробразовать к типу Int32, попробуйте снова.");
+            } while (true);
+        }
+    }
+}
