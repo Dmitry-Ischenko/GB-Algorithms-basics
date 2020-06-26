e38e3c4e2c5e0894f208a16b2b9b250cdffc073b
dmitry <dmitry@dtech-ural.ru>
2020-06-26 13:27:26 +0500

    task1 completed

---
+++ Lesson6/task1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Выполнил Дмитрий Ищенко
+            // 1. Реализовать простейшую хеш-функцию. На вход функции подается строка, на выходе сумма кодов символов.
+            Console.Write("Введите строку: ");
+            String inputString = Console.ReadLine();
+            int summ = 0;
+            foreach (var item in inputString)
+            {
+                summ += (int)item;
+            }
+            Console.WriteLine($"Хеш строки [{inputString}] равен {summ}");
+            Console.ReadKey();
+
+        }
+    }
+}
