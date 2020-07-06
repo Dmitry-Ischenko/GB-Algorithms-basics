b60dddcea40fee2a4c0fb20b7f98fff1d4b5d30c
dmitry <dmitry@dtech-ural.ru>
2020-07-06 10:20:55 +0500

    task1, task2 completed

---
+++ Lesson8/task1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Выполнил Ищенко Дмитрий
+            // 1. Реализовать сортировку подсчетом.
+            Random rand = new Random();
+            int[] array = new int[rand.Next(10,100)];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rand.Next(0,100);
+            }
+
+            Console.WriteLine("Отсортированный массив: {0}", string.Join(", ", CountingSort(array, array.Min(), array.Max()))) ;
+            Console.ReadKey();
+        }
+        static int[] CountingSort(int[] array, int min,int max)
+        {
+            int[] count = new int[max - min + 1];
+            int z = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                count[array[i] - min]++;
+            }
+
+            for (int i = min; i <= max; i++)
+            {
+                while (count[i - min]-- > 0)
+                {
+                    array[z] = i;
+                    z++;
+                }
+            }
+
+            return array;
+        }
+
+    }
+}
---
+++ Lesson8/task2/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Выполнил Ищенко Дмитрий
+            // 2. Реализовать быструю сортировку.
+
+            Random rand = new Random();
+            int[] array = new int[rand.Next(10, 100)];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rand.Next(int.MinValue, int.MaxValue);
+            }
+            QuickSort(array);
+            Console.WriteLine(string.Join(", ", array));
+            Console.ReadKey();
+        }
+        static int[] QuickSort(int[] array, int minIndex, int maxIndex)
+        {
+            if (minIndex >= maxIndex)
+            {
+                return array;
+            }
+
+            var pivotIndex = Partition(array, minIndex, maxIndex);
+            QuickSort(array, minIndex, pivotIndex - 1);
+            QuickSort(array, pivotIndex + 1, maxIndex);
+
+            return array;
+        }
+        static int Partition(int[] array, int minIndex, int maxIndex)
+        {
+            var pivot = minIndex - 1;
+            for (var i = minIndex; i < maxIndex; i++)
+            {
+                if (array[i] < array[maxIndex])
+                {
+                    pivot++;
+                    Swap(ref array[pivot], ref array[i]);
+                }
+            }
+
+            pivot++;
+            Swap(ref array[pivot], ref array[maxIndex]);
+            return pivot;
+        }
+        static void Swap(ref int x, ref int y)
+        {
+            var t = x;
+            x = y;
+            y = t;
+        }
+        static int[] QuickSort(int[] array)
+        {
+            return QuickSort(array, 0, array.Length - 1);
+        }
+    }
+}
